5d77d3b65f9ae25bb335ef9fcc4f6dec723c0b19
abduraxim <admabbasap@gmail.com>
2022-12-06 23:33:45 +0500

    the end

---
+++ adminstratsiya/admin.js
@@ -0,0 +1,16 @@
+function admin() {
+    let a = prompt('log in kiriting')
+    if(a == 'admin'){
+        let b = prompt('parolni kiriting') 
+    }
+    if (a == 'professional dasturchi') {
+        alert('xush kelibsiz')
+    }
+    if(a !=a   ){
+        alert('notogri malumot')
+    }
+    else{
+        alert('bekor qilindi')
+    }
+}
+admin()
---
+++ darsga kech qolish/dars.js
@@ -0,0 +1,9 @@
+function dars() {
+    let a = prompt('darsingiz 08:00 da boshlanadi iltmos daqiani kiritng:        08:')
+    if (a=='00') {
+        alert('darsga kirishingiz mumkin')
+    }else{
+        alert('ertaroq xarakat qilish kerak edi endi kech boldi')
+    }
+}
+dars();
