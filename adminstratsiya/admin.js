8caae4f10d9ffcef190aa46a08115878b82d3827
abduraxim <admabbasap@gmail.com>
2022-12-06 23:58:00 +0500

    admin

--- adminstratsiya/admin.js
+++ adminstratsiya/admin.js
@@ -2,6 +2,7 @@ function admin() {
     let a = prompt('log in kiriting')
     if(a == 'admin'){
         let b = prompt('parolni kiriting') 
+        alert(b)
     }
     if (a == 'professional dasturchi') {
         alert('xush kelibsiz')
